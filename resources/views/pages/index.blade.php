960d11a29ad8157d661deebb0bef85c84f415b87
Hulk <a.kubarev2013@yandex.ru>
2019-10-20 20:58:56 +0300

    blog laravel

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="jumbotron text-center">
+            <h1>Расскажите о своих увлечениях</h1>
+                <h2>Просматривайте блог других людей</h2>
+            <p>Чтобы создать блог, потребуется лишь несколько секунд.</p>
+            <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+        </div>
+@endsection      
+
